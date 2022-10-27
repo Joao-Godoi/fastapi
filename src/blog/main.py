b07811d454f7287952197535dc19dbf8f6c4b2c2
João Godoi <jaoo.godoi@gmail.com>
2022-10-26 22:10:22 -0300

    refactor: Added responses models on endpoints

--- src/blog/main.py
+++ src/blog/main.py
@@ -1,23 +1,24 @@
 from fastapi import Depends, FastAPI, status, HTTPException
 from sqlalchemy.orm import Session
-from typing import Optional
+from typing import List
 
 from src.blog.database import engine, get_db
 from src.blog.models import Base, BlogModel
-from src.blog.schemas import BlogRequest
+from src.blog.schemas.requests import BlogRequest
+from src.blog.schemas.responses import BlogResponse, BlogDetailsResponse
 
 
 app = FastAPI()
 Base.metadata.create_all(engine)
 
 
-@app.get('/blogs')
+@app.get('/blogs', response_model=List[BlogResponse])
 def list(db: Session = Depends(get_db)):
     blogs = db.query(BlogModel).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=BlogDetailsResponse)
 def retrieve(id: int, db: Session = Depends(get_db)):
     blog = db.query(BlogModel).get(ident=id)
     if not blog:
@@ -26,15 +27,13 @@ def retrieve(id: int, db: Session = Depends(get_db)):
     return blog
 
 
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, response_model=BlogDetailsResponse)
 def create(request: BlogRequest, db: Session = Depends(get_db)):
     new_blog = BlogModel(title=request.title, content=request.content)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
-
-    return {"message": "Blog is created successfully!", "title": new_blog.title,
-            "content": new_blog.content}
+    return new_blog
 
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
@@ -44,11 +43,11 @@ def destroy(id: int, db: Session = Depends(get_db)):
     return True
 
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, response_model=BlogDetailsResponse)
 def update(id: int, request: BlogRequest, db: Session = Depends(get_db)):
-    blog = db.query(BlogModel).get(ident=id)
-    if not blog:
+    blog = db.query(BlogModel).filter(BlogModel.id == id)
+    if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found to update")
     blog.update({"title": request.title, "content": request.content})
     db.commit()
-    return {"detail": "updated successfully"}
+    return blog.first()
